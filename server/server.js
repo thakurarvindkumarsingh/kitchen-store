63f1358543c916928f453515a4b4a85326696082
thakurarvindkumarsingh <arvindkumarsingh1170@gmail.com>
2026-05-07 22:40:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
@@ -10,17 +12,22 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 
 app.use('/uploads', express.static('uploads'));
-
+// 👇 IMPORTANT LINE
 const productRoutes = require("./routes/productRoutes");
-app.use("/products", productRoutes);
 
-const orderRoutes = require("./routes/orderRoutes");
-app.use("/orders", orderRoutes);
+
+// 👇 CONNECT ROUTE
+app.use("/products", productRoutes);
 
 // test route
 app.get("/", (req, res) => {
   res.send("Server is running 🔥");
 });
+
+//order route
+const orderRoutes = require("./routes/orderRoutes");
+
+app.use("/orders", orderRoutes);
 
 // DB connect
 mongoose.connect(process.env.MONGO_URI)
@@ -35,7 +42,18 @@ mongoose.connect(process.env.MONGO_URI)
     console.log("DB Error ❌", err);
   });
 
-// ❌ YE HATA DO
-// app.listen(5000, () => {
-//   console.log("Server started on port 5000");
-// });
+app.get("/add-test", async (req, res) => {
+  const Product = require("./models/Product");
+
+  const newProduct = new Product({
+    name: "Test Tawa",
+    price: 400,
+    images: "test.jpg",
+    description: "Testing product",
+    category: "Kitchen"
+  });
+
+  await newProduct.save();
+
+  res.send("Test Product Added ✅");
+});
